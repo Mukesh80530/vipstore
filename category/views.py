405bd82c4c38429a0d53dbcd47829a05b0bb5842
Mukesh80530 <mukeshgautam.er@gmail.com>
2022-02-22 09:38:09 +0530

    Removed pagination

--- category/views.py
+++ category/views.py
@@ -12,23 +12,42 @@ def dashboard(request):
     return render(request, 'dashboard.html', context)
 
 
+# @login_required()
+# def category(request):
+#     sub_cat_id = request.GET.get('cat_id')
+#     product_list = Product.objects.filter(sub_category=sub_cat_id)
+#     if len(product_list):
+#
+#         paginator = Paginator(product_list, 5)
+#
+#         page_number = request.GET.get('page')
+#         page_obj = paginator.get_page(page_number)
+#
+#         context = {
+#             'page_obj': page_obj
+#         }
+#         return render(request, 'sub-category.html', context)
+#     else:
+#         return render(request, 'coming_soon.html')
+
 @login_required()
 def category(request):
     sub_cat_id = request.GET.get('cat_id')
     product_list = Product.objects.filter(sub_category=sub_cat_id)
     if len(product_list):
 
-        paginator = Paginator(product_list, 10)
-
-        page_number = request.GET.get('page')
-        page_obj = paginator.get_page(page_number)
+        # paginator = Paginator(product_list, 5)
+        #
+        # page_number = request.GET.get('page')
+        # page_obj = paginator.get_page(page_number)
 
         context = {
-            'page_obj': page_obj
+            'page_obj': product_list
         }
         return render(request, 'sub-category.html', context)
     else:
         return render(request, 'coming_soon.html')
+
 
 def about(request):
     context = {
